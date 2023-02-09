85430d121b903c9ceb69f94d69cce403ec7ec554
irmalons <irmalons@ucm.es>
2023-02-09 09:47:17 +0100

    04

--- 04.py
+++ 04.py
@@ -14,29 +14,29 @@ def is_anybody_inside(critical, tid):
 def task(common, tid, critical, turn):
     a = 0
     for i in range(10):
-        print(f’{tid}−{i}: Non−critical Section’)
+        print(f'{tid}−{i}: Non−critical Section')
         a += 1
-        print(f’{tid}−{i}: End of non−critical Section’)
+        print(f'tid}−{i}: End of non−critical Section')
         critical[tid] = 1
         while is_anybody_inside(critical, tid):
             critical[tid] = 0
-            print(f’{tid}−{i}: Giving up’)
+            print(f'{tid}−{i}: Giving up')
             while turn.value==tid:
                 pass
             critical[tid] = 1
-        print(f’{tid}−{i}: Critical section’)
+        print(f'{tid}−{i}: Critical section')
         v = common.value + 1
-        print(f’{tid}−{i}: Inside critical section’)
+        print(f'{tid}−{i}: Inside critical section')
         common.value = v
-        print(f’{tid}−{i}: End of critical section’)
+        print(f'{tid}−{i}: End of critical section')
         critical[tid] = 0
         turn.value = tid
         
 def main():
     lp = []
-    common = Value(’i’, 0)
-    critical = Array(’i’, [0]*N)
-    turn = Value(’i’, 0)
+    common = Value('i', 0)
+    critical = Array('i, [0]*N)
+    turn = Value('i', 0)
     for tid in range(N):
         lp.append(Process(target=task, args=(common, tid, critical, turn)))
     print (f"Valor inicial del contador {common.value}")
