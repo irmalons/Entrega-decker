1537733121da5fdc008335cd9a54f397b618025c
Gersac <124214088+Gersac@users.noreply.github.com>
2023-02-09 09:45:12 +0100

    Update 01.py

--- 01.py
+++ 01.py
@@ -6,21 +6,21 @@ N = 8
 def task(common, tid, turn):
    a = 0
    for i in range(100):
-      print(f’{tid}−{i}: Non−critical Section’)
+      print(f'{tid}−{i}: Non−critical Section')
       a += 1
-      print(f’{tid}−{i}: End of non−critical Section’)
+      print(f'{tid}−{i}: End of non−critical Section')
       while turn.value!=tid:
          pass
-      print(f’{tid}−{i}: Critical section’)
+      print(f'{tid}−{i}: Critical section')
       v = common.value + 1
-      print(f’{tid}−{i}: Inside critical section’)
+      print(f'{tid}−{i}: Inside critical section')
       common.value = v
-      print(f’{tid}−{i}: End of critical section’)
+      print(f'{tid}−{i}: End of critical section')
       turn.value = (tid + 1) % N
 def main():
    lp = []
-   common = Value(’i’, 0)
-   turn = Value(’i’, 0)
+   common = Value('i', 0)
+   turn = Value('i', 0)
    for tid in range(N):
       lp.append(Process(target=task, args=(common, tid, turn)))
    print (f"Valor inicial del contador {common.value}")
